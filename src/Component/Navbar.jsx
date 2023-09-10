c6952d0d7c8b178eab2c831e5c2584f917ef900d
unknown <safayetnurelectra@gmail.com>
2023-09-10 23:50:51 +0600

    navbar and number01 food card design with raw css

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,7 +1,25 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import  { useState } from "react";
+import "./Navbar.css";
 
-export default Navbar
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="Navbar">
+      <span className="nav-logo">Anonymous</span>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <a href="/">Food Card</a>
+        <a href="/notify">Notification</a>
+        <a href="/gridView">Grid View</a>
+        <a href="/reactSliderCard">React Slider Card</a>
+      </div>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="bar"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/Pages/FoodCardPage/FoodCard.jsx
+++ src/Pages/FoodCardPage/FoodCard.jsx
@@ -1,8 +1,34 @@
-
+import './foodCard.css';
 const FoodCard = () => {
   return (
-    <div>FoodCard</div>
+    <div className='body-class'>  
+ <div className="food-card">
+    <div className='card-image'>
+    </div>
+     
+    
+    <div className="description">
+     <div className='card-header'> <h2 className='card-h'>Pizza Vegelate 👌</h2>
+     <div className='button'>
+      <button>#VEGETERIAN</button>
+      <button>#ITALIAN</button>
+     </div>
+     </div>
+     
+      <p className='para'>Yammy veggie pizza with tasty olivs, crisp, peppers, fresh arugula and original italian tomator sauce.</p>
+     
+     <div className='card-footer'>
+      <h3>🍕 850 KCL</h3>
+       <h3>🕜 30 MINS</h3>
+        <h3>⭐ 4.7/5</h3>
+     </div>
+    </div>
+    <div className='card-price'>
+    <p>  $11.99</p>
+    </div>
+  </div>
+  </div>
   )
 }
 
-export default FoodCard
+export default FoodCard;
